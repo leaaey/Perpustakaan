65ff717e15dd9af64eba6ee3d8e40746bc656ebe
Mahdalia <mahdalia956@gmail.com>
2024-11-29 02:57:55 +0000

    Initial commit

---
+++ perpus/app/Models/kategori.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kategori extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kategoribuku';
+    protected $primaryKey = 'KategoriID';  
+    protected $fillable = [
+        'NamaKategori',
+        'KategoriID'
+    ];
+    public $timestamps=false;
+}
